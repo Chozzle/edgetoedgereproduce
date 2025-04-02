91c6c1c3e3e6d2a6abbb46c4a5da3fdb64e79c06
Carson Holzheimer <carsonholzy@gmail.com>
2025-04-02 17:36:57 +1000

    It's not related to Fragments

--- app/src/main/java/com/example/edgetoedgeedittext/MainActivity.kt
+++ app/src/main/java/com/example/edgetoedgeedittext/MainActivity.kt
@@ -1,30 +1,42 @@
 package com.example.edgetoedgeedittext
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        val view = findViewById<View>(R.id.fragmentContainer)
-        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout())
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             WindowInsetsCompat.CONSUMED
         }
 
-        // Add fragment
-        supportFragmentManager.beginTransaction()
-            .replace(view.id, TestFragment())
-            .commit()
+        val composeView = findViewById<ComposeView>(R.id.composeView)
+        composeView.setContent {
+            Scaffold(topBar = {
+                TopAppBar(title = { Text("Title") })
+            }) { paddingValues ->
+                Text("Hello", modifier = Modifier.padding(paddingValues))
+            }
+        }
 
-
+        // To reproduce, run on Android API 34 and change theme from Light to Dark mode.
     }
 }
